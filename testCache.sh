665b7d4ff301ecd46dcf05d9509c208c9a4ff6e0
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-11 22:52:36 +0200

    test split cache

--- testCache.sh
+++ testCache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -a "/home/travis/cache/coucou.txt" ]; then
+if [ ! -f "/home/travis/cache/coucou.txt" ]; then
   echo "create coucou"
   mkdir -p /home/travis/cache/
   echo "$JHIPSTER" > /home/travis/cache/coucou.txt
