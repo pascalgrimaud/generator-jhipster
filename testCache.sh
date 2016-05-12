0a40476168d03d237d070a28879aa6ca66e92dc2
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-05-12 08:11:40 +0200

    test cache

--- testCache.sh
+++ testCache.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -f "/home/travis/cache/coucou.txt" ]; then
-  echo "create coucou.txt"
+if [ $JHIPSTER == "app-default-from-scratch" ]; then
+  echo "create mycache.txt"
   mkdir -p /home/travis/cache/
-  echo "$JHIPSTER" > /home/travis/cache/coucou.txt
+  echo "$JHIPSTER $(date)" > /home/travis/cache/mycache.txt
 fi
-cat /home/travis/cache/coucou.txt
+cat /home/travis/cache/mycache.txt
